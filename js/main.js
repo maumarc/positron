144eb438c7b554e70f54926f57d0951c5d7cc537
Mauricio Marcondes Soerensen <mauriciosoerensen@gmail.com>
2019-10-29 15:53:23 -0300

    mobile

--- js/main.js
+++ js/main.js
@@ -1,25 +1,37 @@
-$('.header__nav__menu__item__link__img').on('click', function(){
-	$('#header__bg').addClass('active')
-})
-$('.header__bg__form__close').on('click', function(){
-	$('#header__bg').removeClass('active')
+// $('.header__nav__menu__item__link__img').on('click', function(){
+// 	$('#header__bg').addClass('active')
+// })
+// $('.header__bg__form__close').on('click', function(){
+// 	$('#header__bg').removeClass('active')
+// })
+
+var lupa = document.querySelector('.header__nav__menu__item__link__img');
+var header = document.querySelector('#header__bg');
+
+lupa.addEventListener('click', function(){
+	header.classList.add('active');
 })
 
-$('.header__nav__menu__item__link__img').on('click', function(){
-	$('#busca').addClass('active')
-	event.preventDefault(); 
-})
-$('.fecharSearch').on('click', function(){
-	$('#busca').removeClass('active')
+document.querySelector('.header__bg__form__close').addEventListener('click', function(){
+	header.classList.remove('active');
 })
 
-$('.footer__ul__item__link__siteMap').on('click', function(){
-	$('#siteMap').addClass('active')
-	event.preventDefault(); 
-})
-$('.fecharSiteMap').on('click', function(){
-	$('#siteMap').removeClass('active')
-})
+
+// $('.header__nav__menu__item__link__img').on('click', function(){
+// 	$('#busca').addClass('active')
+// 	event.preventDefault(); 
+// })
+// $('.fecharSearch').on('click', function(){
+// 	$('#busca').removeClass('active')
+// })
+
+// $('.footer__ul__item__link__siteMap').on('click', function(){
+// 	$('#siteMap').addClass('active')
+// 	event.preventDefault(); 
+// })
+// $('.fecharSiteMap').on('click', function(){
+// 	$('#siteMap').removeClass('active')
+// })
 
 // $('.nav-menu').on('mouseover', function(){
 // 	$('.nav-menu-hidden').addClass('active')
